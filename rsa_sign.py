40c207a49021cbabf8482d38e6e28da02103b533
ArjunSom8 <arjunsom29@gmail.com>
2023-12-04 19:08:35 +0000

    Fixed code

--- rsa_sign.py
+++ rsa_sign.py
@@ -2,8 +2,10 @@ from Crypto.Signature import PKCS1_v1_5
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 
+passphrase = 'dees'
+
 with open('private.pem', 'rb') as f:
-    pkey = RSA.importKey(f.read())
+    pkey = RSA.importKey(f.read(), passphrase=passphrase)
 m = b'Hello world!'
 hash = SHA256.new(m)
 pk = PKCS1_v1_5.new(pkey)
